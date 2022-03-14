4cff75665e31d0208057c9fe7222438af036f57c
Kevin Martinez <kevinvr@hotmail.es>
2022-03-13 23:34:00 -0600

    pause at end

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ mod types;
 use rand::prelude::*;
 use std::env;
 use types::Node;
+use std::io;
+use std::io::prelude::*;
+
+fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -51,4 +65,5 @@ fn main() {
             println!("Sin solución")
         }
     };
+    pause();
 }
